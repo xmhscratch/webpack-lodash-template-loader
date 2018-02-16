bc0a29c25bd0dad379b30dc8d0110a407cc3c4ca
xmhscratch <xmhscratch@gmail.com>
2018-02-16 21:48:48 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+var loaderUtils = require('loader-utils');
+
+module.exports = function(source) {
+  this.cacheable && this.cacheable();
+  var query = loaderUtils.parseQuery(this.query);
+  var options = this.options.ejsLoader || {};
+
+  ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
+    var setting = query[templateSetting];
+    if (_.isString(setting)) {
+      query[templateSetting] = new RegExp(setting, 'g');
+    }
+  });
+
+  return export default _.template(source, _.extend({}, query, options));
+};
