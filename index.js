7225ca5d6d80b0f7331fc703cb4a9f33482b0413
xmhscratch <xmhscratch@gmail.com>
2018-02-16 22:25:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var _ = require('lodash');
 var loaderUtils = require('loader-utils');
 
 function getLoaderConfig(context) {
-	var query = loaderUtils.getOptions(context) || {};
-	var configKey = query.config || 'lodashLoader';
-	var config = context.options && context.options.hasOwnProperty(configKey) ? context.options[configKey] : {};
+    var query = loaderUtils.getOptions(context) || {};
+    var configKey = query.config || 'lodashLoader';
+    var config = context.options && context.options.hasOwnProperty(configKey) ? context.options[configKey] : {};
 
-	delete query.config;
+    delete query.config;
 
-	return assign(query, config);
+    return assign(query, config);
 }
 
-module.exports = function(source) {
-  this.cacheable && this.cacheable();
-  var config = getLoaderConfig(this);
+module.exports = function (source) {
+    this.cacheable && this.cacheable();
+    var config = getLoaderConfig(this);
 
-  ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
-    var setting = config[templateSetting];
-    if (_.isString(setting)) {
-      config[templateSetting] = new RegExp(setting, 'g');
+    ['escape', 'interpolate', 'evaluate'].forEach(function (templateSetting) {
+        var setting = config[templateSetting];
+        if (_.isString(setting)) {
+            config[templateSetting] = new RegExp(setting, 'g');
+        }
+    });
+
+    var template = _.template(source, config.templateSetting);
+    var exportsString = "module.exports = ";
+    if (config.exportAsDefault) {
+        exportsString = "exports.default = ";
+    } else if (config.exportAsEs6Default) {
+        exportsString = "export default ";
     }
-  });
-
-  var template = _.template(source, config.templateSetting);
-  var exportsString = "module.exports = ";
-  if (config.exportAsDefault) {
-    exportsString = "exports.default = ";
-  } else if (config.exportAsEs6Default) {
-    exportsString = "export default ";
-  }
-  return `${exportsString} ${template}`;
+    return `${exportsString} ${template}`;
 };
